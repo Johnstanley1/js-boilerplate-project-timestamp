67d347de1e515971d935031fcc4a7f09f9bb60ef
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2025-10-25 13:38:34 -0400

    Add /api/whoami endpoint and update UI for header parser
    Implemented the /api/whoami endpoint to return client IP address, language, and software information. Updated index.html and style.css to reflect the new Request Header Parser Microservice, replacing the previous Timestamp Microservice content. Added dotenv dependency for environment variable support.

--- index.js
+++ index.js
@@ -2,47 +2,84 @@
 // where your node app starts
 
 // init project
+// var express = require('express');
+// var app = express();
+
+// // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// // so that your API is remotely testable by FCC 
+// var cors = require('cors');
+// app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// // http://expressjs.com/en/starter/static-files.html
+// app.use(express.static('public'));
+
+// // http://expressjs.com/en/starter/basic-routing.html
+// app.get("/", function (req, res) {
+//   res.sendFile(__dirname + '/views/index.html');
+// });
+
+
+// // your first API endpoint... 
+// app.get("/api/hello", function (req, res) {
+//   res.json({greeting: 'hello API'});
+// });
+ 
+
+// app.get("/api/:date?", function(req, res) {
+//   let dateParam = req.params.date;
+//   let date;
+  
+//   if (!dateParam){
+//     date = new Date();
+//   }else if(!isNaN(Number(dateParam))){
+//     date = new Date(Number(dateParam))
+//   }else{
+//     date = new Date(dateParam)
+//   }
+  
+//   if(date.toString() === "Invalid Date"){
+//       res.json({error: "Invalid Date"})
+//   }
+       
+//   res.json({unix: date.getTime(), utc: date.toUTCString()})
+// })
+
+// init project
+require('dotenv').config();
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
 });
- 
 
-app.get("/api/:date?", function(req, res) {
-  let dateParam = req.params.date;
-  let date;
-  
-  if (!dateParam){
-    date = new Date();
-  }else if(!isNaN(Number(dateParam))){
-    date = new Date(Number(dateParam))
-  }else{
-    date = new Date(dateParam)
-  }
-  
-  if(date.toString() === "Invalid Date"){
-      res.json({error: "Invalid Date"})
-  }
-       
-  res.json({unix: date.getTime(), utc: date.toUTCString()})
-})
+
+app.get('/api/whoami', (req, res) => {
+  const ipAddress = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({
+    ipaddress: ipAddress,
+    language: language,
+    software: software
+  });
+});
+
 
 
 // Listen on port set in environment variable or default to 3000
